95f5f3608590f63023c72fca9c18f6a955941000
emilknievel <emil.vagstedt@gmail.com>
2012-12-19 12:45:02 +0100

    Added a notes file

--- src/se/liu/ida/emiva760/tddc69/projekt/GameTest.java
+++ src/se/liu/ida/emiva760/tddc69/projekt/GameTest.java
@@ -10,15 +10,15 @@ package se.liu.ida.emiva760.tddc69.projekt;
  */
 
 public class GameTest {
-	public static void main(String[] args) {
-		// Test the coordinates
-
-
-		// Test the drawing methods
-
-
+    public static void main(String[] args) {
+	// Test the coordinates
+	
+	
+	// Test the drawing methods
+	
+	
         // Test the physics
-
-
-	}
+	
+	
+    }
 }
