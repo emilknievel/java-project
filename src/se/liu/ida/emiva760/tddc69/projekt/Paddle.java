42d4b77be5d1341740e22cb2d74f34074b844ec6
emilknievel <emil.vagstedt@gmail.com>
2012-12-22 16:53:02 +0100

    Added paddle class and fixed enum.

---
+++ src/se/liu/ida/emiva760/tddc69/projekt/Paddle.java
@@ -0,0 +1,11 @@
+package se.liu.ida.emiva760.tddc69.projekt;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: emil
+ * Date: 12/22/12
+ * Time: 4:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Paddle {
+}
